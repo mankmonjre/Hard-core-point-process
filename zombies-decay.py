801993a0f4a80eecc000a7c22e9f0e485c333e6f
Mayank Manjrekar <mankmonjre@gmail.com>
2018-12-17 15:37:10 -0600

    Improved readability of the zombies_decay.py

--- zombies-decay.py
+++ zombies-decay.py
@@ -1,61 +1,77 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 """
-Created on Thu Aug 25 11:12:55 2016
+@author: Mayank Manjrekar
 
-@author: mayankm
+The hard-core birth-death process is a model where balls of radius one
+arrive at random, and are accepted if thare no overlapping balls
+present in the system. Every point has an exponential lifetime.
 
-This is a simulation for a coupling of a system where the hard-core 
-balls arrive at random, and are accepted if there are no competing balls
-are present in a radius 1 from it. Every point accepted has an exponential
-lifetime.
+In this SimPy simulation, we run a bi-simulation for this
+process, i.e., a simultaneous simulation of two processes, starting
+from different initial conditions, but driven by the same
+arrivals. This kind of simulation will be useful in understanding the
+mixing times of the process.
+
+The particles only present in the process-0 are called zombies (BLUE), and
+those present only in process-1 are called anti-zombies (GREEN). Those
+that are present in both are called regular (RED).
 """
+
 import matplotlib.animation as manimation
 import simpy
 import numpy as np
 import matplotlib.pyplot as plt
-
-SIZE = 20
-LAMBDA = 2
-GEN_TIME_CONST = 1 / (LAMBDA * SIZE**2)
-LIFETIME = 0.125
-UNTIL_TIME = 180
-NUMZOMBIES = 5
-
-
-
 exprand = np.random.exponential
-
 rand = np.random.random
 
+
+
+DIM = 2
+SIZE = 20   # Size of the domain is (SIZE)^DIM
+LAMBDA = 2   # The arrival rate
+GEN_TIME_CONST = 1 / (LAMBDA * SIZE**2)   # Total arrival rate
+LIFETIME = 0.125    # The average time a particle stays
+UNTIL_TIME = 180    # The run-time of the simulation
+NUMZOMBIES = 5      # Maximum number of zombies initially present in
+                    # the system
 ALIVE = 1
 DEAD = 0
-TORUS = SIZE * np.array([[i, j] for i in range(-1, 2) for j in range(-1, 2)])
+
+
+TORUS = []
+for i in range(DIM):
+    temp = [[k+[j]] for k in TORUS for j in range(-1,2)]
+    TORUS = temp
+TORUS = SIZE * np.array(TORUS)
 
 CAPTURE_MOVIE = True
-UPDATE_PLOT = True
+UPDATE_PLOT = False
 
-class Point():
+# Particle class holds the information of a particle
+class Particle():
     def __init__(self, iden, x_p, b_p, l_p, s_p):
         self.id = iden
         self.location = x_p
         self.birth_time = b_p
         self.lifetime = l_p
-        self.state = s_p
+        self.state = s_p     # State = Vector of ALIVE or DEAD in
+                             # process_{0,1}
         env.process(self.run())
         
-    def run(self):
+    def run(self): # The lifetime process of a particle. It waits till
+                   # the lifetime runs out.
         yield env.timeout(self.lifetime)
-        hashed_loc = np.floor(self.location)
+        hash_loc = np.floor(self.location)
         self.state = [DEAD, DEAD]
-        t_hashed_loc = tuple(hashed_loc)
-        points[t_hashed_loc].remove(self) 
-        print("{0:3f} : Point {1} departs".format(env.now, self.id))
+        t_hash_loc = tuple(hash_loc)
+        Plist[t_hash_loc].remove(self) 
+        print("{0:3f} : Particle {1} departs".format(env.now, self.id))
         
 
-def hashloc(loc):
-    return np.floor(loc) % SIZE
+#def hashloc(loc):
+#    return np.floor(loc) % SIZE
 
-def grid(ind, hashed_loc):
+def adjSquares(ind, hashed_loc):
     if ind == 0:
         rt = []
         for i in range(-1,2):
@@ -68,7 +84,7 @@ def grid(ind, hashed_loc):
         for i in range(-1,2):
             temp = list(hashed_loc)
             temp[ind] += i
-            rt = rt + grid(ind - 1, temp)
+            rt = rt + adjSquares(ind - 1, temp)
         return rt
 
 
@@ -76,13 +92,14 @@ def distance(loc1, loc2):
     return min(np.linalg.norm((TORUS + loc1 - loc2), axis=1))
 
 
-def state_at_arrival(loc):
+# Find the state of a particle after arrival
+def stateAtArrival(loc): 
     state = [ALIVE, ALIVE]
-    hashed_loc = hashloc(loc)
+    hash_loc = np.floor(loc)
     ind = len(loc) - 1
-    for p_bucket in grid(ind, hashed_loc):
-        if tuple(p_bucket) in points:
-            for pt in points[tuple(p_bucket)]:
+    for p_bucket in adjSquares(ind, hash_loc):
+        if tuple(p_bucket) in Plist:
+            for pt in Plist[tuple(p_bucket)]:
                 if distance(pt.location, loc) < 1:
                     if pt.state[0] == ALIVE:
                         state[0] = DEAD
@@ -92,57 +109,57 @@ def state_at_arrival(loc):
                         return state
     return state
 
-
-def point_gen_proc():
+# The particle generator process
+def particleGenerator():
     iden = 1
+    # First we generate the Zombies at time 0
     for i in range(NUMZOMBIES):
         loc = np.array([rand(), rand()]) * SIZE
-        hashed_loc = hashloc(loc)
-        state = state_at_arrival(loc)
+        hash_loc = np.floor(loc) % SIZE
+        state = stateAtArrival(loc)
         state[0] = DEAD
         if state[1] == ALIVE:
-            print("{0:3f} : Point {1} arrives as a Zombie".format(env.now, iden, state))
-            new_pt = Point(iden, loc, env.now, exprand(1 / LIFETIME), state)
-            t_hashed_loc = tuple(hashed_loc)
-            if t_hashed_loc in points:
-                points[t_hashed_loc].append(new_pt)
+            print("{0:3f} : Particle {1} arrives as a Zombie".format(env.now, iden, state))
+            new_pt = Particle(iden, loc, env.now, exprand(1 / LIFETIME), state)
+            t_hash_loc = tuple(hash_loc)
+            if t_hash_loc in Plist:
+                Plist[t_hash_loc].append(new_pt)
             else:
-                points[t_hashed_loc] = [new_pt]
+                Plist[t_hash_loc] = [new_pt]
             iden += 1
     while True:
-        yield env.timeout(exprand(GEN_TIME_CONST))
+        yield env.timeout(exprand(GEN_TIME_CONST)) # Wait till new arrival
         loc = np.array([rand(), rand()]) * SIZE
-        hashed_loc = np.floor(loc)
-        state = state_at_arrival(loc)
+        hash_loc = np.floor(loc) % SIZE
+        state = stateAtArrival(loc)
         if state[0] == ALIVE or state[1] == ALIVE:
-            print("{0:3f} : Point {1} arrives with state {2} end_sim = {3}".format(env.now, iden, state, end_sim))
-            new_pt = Point(iden, loc, env.now, exprand(1 / LIFETIME), state)
-            t_hashed_loc = tuple(hashed_loc)
-            if t_hashed_loc in points:
-                points[t_hashed_loc].append(new_pt)
+            print("{0:3f} : Particle {1} arrives with state {2}, end_sim = {3}".format(env.now, iden, state, end_sim))
+            new_pt = Particle(iden, loc, env.now, exprand(1 / LIFETIME), state)
+            t_hash_loc = tuple(hash_loc)
+            if t_hash_loc in Plist:
+                Plist[t_hash_loc].append(new_pt)
             else:
-                points[t_hashed_loc] = [new_pt]
+                Plist[t_hash_loc] = [new_pt]
         iden += 1
-        if end_sim:
+        if end_sim: # Another way to safely exit the simulation
             env.exit()
         
 
 
 def colorpt(state):
     if state[0]==state[1]==ALIVE:
-        return [1,0,0,1]
-    elif state[0]==ALIVE:
-        return [0,1,0,1]
+        return [1,0,0,1] #RED
+    elif state[0]==ALIVE: 
+        return [0,1,0,1] #BLUE
     else:
-        return [0,0,1,1]
+        return [0,0,1,1] #GREEN
 
 
 def update_plot():
-    #colors = []
     plt.cla()
     global end_sim
     end_sim = True
-    for bucket in points.values():
+    for bucket in Plist.values():
         for pt in bucket:
             ax1.add_artist(plt.Circle(pt.location, 0.5,alpha = 0.3, color = colorpt(pt.state)))   
             if pt.state[0] != pt.state[1] and end_sim:
@@ -151,11 +168,6 @@ def update_plot():
     if UPDATE_PLOT:
         plt.draw()
         plt.pause(0.0001)
-
-    
-    #plt.draw()
-    #plt.pause(0.00001)
-            
     
 
 def writer_grabber():
@@ -170,26 +182,19 @@ def writer_grabber():
                 writer.grab_frame()
             if end_sim:
                 env.exit()
-            
 
-
-
-
+                
 def main():
     np.random.seed(0)
-    env.process(point_gen_proc())
-    env.process(writer_grabber())
+    env.process(particleGenerator()) # Start Particle generator
+    env.process(writer_grabber())    # Video capture process
     env.run(until = UNTIL_TIME)
 
 
-
+# Setting up the SimPy simulation
 env = simpy.Environment()
-points = dict()
+Plist = dict()
 fig1, ax1 = plt.subplots()
 plt.axis('equal')
-#plt.show()
 end_sim = False
-#plt.axes().set_aspect('equal', 'datalim')
-
-
 main()
